e25b4e835226c7a8a4547bfb98c260d447c3ab1d
The Underdog <65360079+fish-hue@users.noreply.github.com>
2025-05-03 08:14:19 -0500

    Create idor_payloads.py

---
+++ scan_modules/payloads/idor_payloads.py
@@ -0,0 +1,8 @@
+idor_payloads = [
+    "1",            # Common user ID
+    "2",            # Next user ID
+    "9999",         # Non-existent user ID
+    "admin",        # Try for admin user ID
+    "0",            # Possibly an invalid user ID
+    "superadmin",   # Guessing privilege escalation with user role names
+]
